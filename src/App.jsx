6df62d0acf93b1a9b7834f8fe59785b0ce75a5a7
Johnson Mao <tutelary.maomao@gmail.com>
2022-11-24 23:17:05 +0800

    feat: 新增基礎配置

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 function App() {
-  return (
-    <div className="App">
-      Test
-    </div>
-  )
+	return (
+		<div>
+			{`\\ 碰 / 我是短衝小精靈 ， 開發 A 組的 PO 。\n
+PO 也就是產品負責人（Product Owner） ， 產品負責人會負責評估產品待辦清單的價值與重要性， 依序排列要執行的優先順序 ， 對齊產品目標 。 最後排出產品待辦清單\n
+（Product Backlog） 唷 ！`}
+		</div>
+	);
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
+	const isDev = mode === 'development';
+
 	return {
-		base: mode === 'development' ? '/' : '/scrum_f2e/',
-		plugins: [react()],
+		base: isDev ? '/' : '/scrum_f2e/',
+		plugins: [svgrPlugin(), react()],
 		resolve: {
 			alias: {
-				'@': '/src'
+				'@': '/src',
+				'@images': '/src/assets/images'
 			}
+		},
+		build: {
+			outDir: 'dist',
+			target: isDev ? 'modules' : 'es2015'
 		},
 		server: {
 			host: '0.0.0.0',
 			open: true
 		}
-	}
+	};
 });
